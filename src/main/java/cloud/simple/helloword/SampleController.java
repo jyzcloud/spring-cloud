d5cdc397c754017899124c13e979d7072b224115
jyzcloud <30612282+jyzcloud@users.noreply.github.com>
2017-10-13 17:33:59 +0800

    wwww

---
+++ src/main/java/cloud/simple/helloword/SampleController.java
@@ -0,0 +1,21 @@
+package cloud.simple.helloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class SampleController {
+	@ResponseBody
+	@RequestMapping(value = "/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleController.class, args);
+	}
+}
